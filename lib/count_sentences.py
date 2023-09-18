d6a20a24b715ab01588a8f58e29e21d1d0a781f3
BenedictKipkuruiLangat <benedictlangat2000@gmail.com>
2023-09-18 21:47:43 +0300

    Done with assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value=None):
+        if not isinstance(value, str):
+            print("The value must be a string.")
+        else:
+            self.value = value
+
+    def is_sentence(self):
+        return self.value.endswith(".")
+
+    def is_question(self):
+        return self.value.endswith("?")
+
+    def is_exclamation(self):
+        return self.value.endswith("!")
+
+    def count_sentences(self):
+        # Split the string using punctuation as separators
+        sentences = self.value.split(".") + self.value.split("?") + self.value.split("!")
+        
+        # Remove empty strings from the list
+        sentences = [sentence.strip() for sentence in sentences if sentence.strip()]
+        
+        return len(sentences)
